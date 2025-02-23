b6496bac3546ad73cbdf12df84de6ad3c735697c
Tomer Cohen <tomerc248@gmail.com>
2025-02-23 19:58:34 +0200

    added backup script, firefox tabs and bootstrap

---
+++ scripts/firefox_tabs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+firefox --new-tab "https://web.whatsapp.com/" \
+        --new-tab "https://claude.ai/" \
+        --new-tab "https://calendar.google.com/calendar/u/1/r/tasks" \
+        --new-tab "https://keep.google.com/u/1/" \
+        --new-tab "https://calendar.google.com/calendar/u/1/r" \
+        --new-tab "https://mail.google.com/mail/u/0/#inbox" \
+        --new-tab "https://meet.google.com/landing" \
