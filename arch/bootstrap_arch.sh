d83d6e5a99b3dc5a358ccf5e8d98889314859e76
Tomer Cohen <129614080+tTomeRr@users.noreply.github.com>
2025-07-12 17:32:07 +0300

    Update bootstrap_arch.sh - removed copyq added gpaste

--- arch/bootstrap_arch.sh
+++ arch/bootstrap_arch.sh
@@ -25,7 +25,7 @@ dev_packages=(
     "nodejs"
     "npm"
     "xclip"
-    "copyq"
+    "gpaste"
 )
 
 driver_packages=(
@@ -97,19 +97,5 @@ EOF
 systemctl --user enable qbittorrent.service
 systemctl --user start qbittorrent.service
 
-# Create autostart directory if it doesn't exist
-mkdir -p ~/.config/autostart
-
-# Create autostart file for CopyQ
-cat > ~/.config/autostart/copyq.desktop << EOF
-[Desktop Entry]
-Type=Application
-Name=CopyQ
-Exec=copyq
-Hidden=false
-NoDisplay=false
-X-GNOME-Autostart-enabled=true
-EOF
-
-# If using CopyQ with Super+ V - Disable GNOME's Super+V notification binding: 
+# If using GPaste with Super+ V - Disable GNOME's Super+V notification binding: 
 gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>m']"
